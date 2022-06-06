d8e37cac37f7343751402d0e2cedbadec26c4730
DarkWake9 <vibhavasu2018@gmail.com>
2022-06-06 10:15:50 +0530

    06062022-10.15_Task1bdone

--- Task1b/histogram/hist.py
+++ Task1b/histogram/hist.py
@@ -1,6 +1,4 @@
-
 import pandas as pd
-import pylab as pl
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -20,10 +18,8 @@ def n_e_hist(file):
     smear = pd.DataFrame(content, columns = column)
     smear
     f2.close()
-    de = [pow(10,float(smear['log10(E_nu/GeV)_max'][i])) - pow(10,float((smear['log10(E_nu/GeV)_min'][i]))) for i in range(0,len(smear['log10(E_nu/GeV)_max']))]
     dloge = [float(smear['log10(E_nu/GeV)_max'][i]) -float((smear['log10(E_nu/GeV)_min'][i])) for i in range(0,len(smear['log10(E_nu/GeV)_max']))]
-    dphi = [float(smear['Dec_nu_max[deg]'][i]) - float((smear['Dec_nu_min[deg]'][i])) for i in range(0,len(smear['Dec_nu_max[deg]']))]
-    
+    de = [pow(10,float(smear['log10(E_nu/GeV)_max'][i])) - pow(10,float((smear['log10(E_nu/GeV)_min'][i]))) for i in range(0,len(smear['log10(E_nu/GeV)_max']))]
     e = [float(de[i])/float(dloge[i]) for i in range(0,len(smear['log10(E_nu/GeV)_max']))]
     
     #eff_a = [float(i) for i in smear['A_Eff[cm^2]']]
@@ -31,26 +27,25 @@ def n_e_hist(file):
     #e2dphide = [float(smear['Fractional_Counts'][i]) * e2[i] * dphi[i]/de[i] for i in range(0,len(smear['log10(E_nu/GeV)_max']))]
     
 
-    yax = [e[i] * dphi[i]/dloge[i] for i in range(0, len(dloge))]
 
     #HISTOGRAMS OF N VS E BINNED IN LOG E
 
     plt.figure(figsize=(26,16))
     plt.rcParams.update({'font.size': 24})
-    plt.hist(yax, bins= np.logspace(3,12,23))
+    plt.hist(e, bins= np.logspace(3,10))
     plt.xscale("log")
     plt.xlabel("log(E_nu/GeV)")
     plt.ylabel("N")
     plt.suptitle(file.replace('icecube_10year_ps/irfs/','').replace('_smearing','').replace('.csv',''))
     plt.grid(True, which="both")
     plt.plot
-    #plt.show()
-    plt.savefig(file.replace('icecube_10year_ps/irfs/','Task1b/histogram/smearing/').replace('.csv','.png'))
+    plt.show()
+    #plt.savefig(file.replace('icecube_10year_ps/irfs/','Task1b/histogram/smearing/').replace('.csv','.png'))
 
 filenames = ["icecube_10year_ps/irfs/IC40_smearing.csv", "icecube_10year_ps/irfs/IC59_smearing.csv",
  "icecube_10year_ps/irfs/IC79_smearing.csv", "icecube_10year_ps/irfs/IC86_I_smearing.csv", "icecube_10year_ps/irfs/IC86_II_smearing.csv"]
 
-for filename in filenames:
-    n_e_hist(filename)
-#n_e_hist(filenames[0])
+#for filename in filenames:
+ #   n_e_hist(filename)
+n_e_hist(filenames[1])
 #LEAST GAPS WITH "E BINNED AS bins = np.logspace(3,12,23))""
