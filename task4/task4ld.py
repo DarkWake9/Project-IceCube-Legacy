a0b46c7fcfe1d4fcf1b52369f82e3a1d82b42e1a
DarkWake9 <vibhavasu2018@gmail.com>
2022-10-31 22:45:12 +0530

    31102022 - 22.44 - TASK 4d, 4e DOCUMENTATION DONE

--- task4/task4ld.py
+++ task4/task4ld.py
@@ -1,73 +1,43 @@
 import numpy as np
 import pandas as pd
-import os
-import multiprocessing as mul
-from multiprocessing import Process
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.optimize import minimize
 
 
-####
-#### IMPORTING AND SPLITTING ICDATA $$$
+def hvovec(lon1, lat1, lon2, lat2, rad=False):
+
+    '''
+    Parameters
+    ----------
+    lon1 : float
+        Longitude of first point.
+    lat1 : float
+        Latitude of first point.
 
 
-path = "/media/darkwake/VIB2/Project-IceCube/icecube_10year_ps/events"
-filenames = ["IC40_exp.csv", "IC59_exp.csv","IC79_exp.csv", "IC86_I_exp.csv", "IC86_II_exp.csv",
-"IC86_III_exp.csv", "IC86_IV_exp.csv", "IC86_V_exp.csv", "IC86_VI_exp.csv", "IC86_VII_exp.csv"]
-file = filenames[0]
-f = open(os.path.join(path, file), 'r')
-lines = f.readlines()
-column=lines[0].split()
-column.pop(0)
-content = []
-for file in filenames:
-    f = open(os.path.join(path, file), 'r')
-    lines = f.readlines()
-    for line in lines[1:]:
-        content.append(line.split())
-    f.close()
-icdata = pd.DataFrame(content, columns=column)
-icdata['log10(E/GeV)'] = [float(i) for i in icdata['log10(E/GeV)']]
+    lon2 : float
+        Longitude of second point.
+    lat2 : float
+        Latitude of second point.
 
+    rad : Boolean, optional
+        (default) False -> Angle is returned in Degrees
+        True -> Angle is returned in radians
+    
 
+    Returns
+    -------
+    float
+        Returns the haversine angle between two vectors given their latitude and longitude
+    
 
-#IMPORTING MSPDATA
-f = open("/media/darkwake/VIB2/Project-IceCube/allpsr.txt", 'r')
-lines = f.readlines()
-
-content=[]
-column=lines.pop(0).split()
-for line in lines[:]:
-    content.append(line.split())
-    #the INITAL DATABASE IS CLUTTERED SO WE REMOVE THE NULL COLUMNS AND OTHER CLUTTER
-mspdata = pd.DataFrame(content).dropna().drop_duplicates()#.drop(range(0,6)).dropna().drop([2,6,8,10,11,13,14], axis=1)
-f.close()
-line = []
-lines = []
-
-mspdata.columns = column
-column = []
-content=[]
-mspdata = mspdata.sort_values('DECJD')
-mspdata.dropna(inplace=True)
-mspdata = mspdata.reset_index()
-mspdata = mspdata.drop(["index", "#"], axis=1)
-icdata
-mspdata
-msra = [float(i) for i in mspdata['RAJD']]
-msdec = [float(i) for i in mspdata['DECJD']]
-icra = [float(i) for i in icdata['RA[deg]']]
-icdec = [float(i) for i in icdata['Dec[deg]']]
-icang = [float(i) for i in icdata['AngErr[deg]']]
-global p, lg
-p = len(msra)
-lg = len(icra) // p + 1
-#cut = 20
-#cone = 3
-#psrno = 0
-
-def hvovec(lon1, lat1, lon2, lat2):
+    Warning
+    -------
+        This function assumes the input to be in degrees and of equal length\n
+        or one of the input pairs to be a single value and the other pair to be an array
+    '''
 
     #Convert decimal degrees to Radians:
     lon1 = np.deg2rad(lon1)
@@ -81,122 +51,170 @@ def hvovec(lon1, lat1, lon2, lat2):
 
     a = np.add(np.multiply(np.sin(lat1), np.sin(lat2)), np.multiply(np.multiply(np.cos(lat1), np.cos(lat2)), np.cos(dlon)))
 
-    return np.abs(np.rad2deg(np.arccos(a)))
-
-
-
-
-def angfinder(b, cut):
-    ang = []
-    for a in range(lg):
-        
-        if a != lg - 1:
-        #try:
-            ilo = icra[a*p:a*p + p]
-            ila = icdec[a*p:a*p + p]
-            lo = msra[b] * np.ones(p)
-            la = msdec[b] * np.ones(p)
-            temp = hvovec(ilo, ila, lo, la)
-            for tt in range(len(temp)):
-                if temp[tt] > cut:
-                    temp[tt] = -1
-            ang.extend(temp)
-        else:
-        #except:
-            ilo = icra[a*p:]
-            ila = icdec[a*p:]
-            ext = len(ilo)
-            lo = msra[b] * np.ones(ext)
-            la = msdec[b] * np.ones(ext)
-            temp = hvovec(ilo, ila, lo, la)
-            #ang.extend(hvovec(ilo, ila, lo, la))
-            for tt in range(len(temp)):
-                if temp[tt] > cut:
-                    temp[tt] = -1
-            ang.extend(temp)
-        
-    return ang
-
-
-#aang = angfinder(psrno)
-
-def S_ij(i, aang, cut):### $P_i[a] = \dfrac{ns[a]}{N} S[a] + \left(1 - \dfrac{ns[a]}{N}\right)B[a]$ if ns 
-        
-    if len(aang) < p*1.1:
-        arr = []
-        for j in range(0,len(icdata)):
-            ang = aang[i][j]
-            if ang != -1:
-                if True and ang < cut:
-                    sg = np.deg2rad(icang[j]) ** 2
-                    ang = np.deg2rad(ang) ** 2
-                    arr.append(np.exp(-1 * ang / (2 * sg)) / (2 * np.pi * sg))
-        return arr
+    if rad == True:
+        return np.arccos(a)
     else:
-        arr = []
-        for j in range(0,len(icdata)):
-            ang = aang[j]
-            if ang != -1:
-                if True and ang < cut:
-                    sg = np.deg2rad(icang[j]) ** 2
-                    ang = np.deg2rad(ang) ** 2
-                    arr.append(np.exp(-1 * ang / (2 * sg)) / (2 * np.pi * sg))
-        return arr
+        return np.abs(np.rad2deg(np.arccos(a)))
 
-#S = S_ij(psrno)
 
-def nsang(aang, cut):
-    Ns = []
-    if len(aang) < p* 1.1:
-        for i in aang:
-            count = 0
-            for j in i:
-                if j != -1:
-                    if j < cut:
-                        count += 1
-            Ns.append(count)
-        Ns = np.array(Ns)
-        return Ns
+def angdecfinder(psrno, declcut):
+
+    '''
+
+    Parameters
+    ----------
+    psrno : int
+        Pulsar number from the ATNF catalogue.
+    
+    declcut : float
+        Declination cut-off in degrees.
+
+
+
+    Returns
+    -------
+    float
+    Returns the absolute difference between the declinations of pulsar {psrno}\n
+    and each of the neutrinos within the specified declcut
+    '''
+    #aang = hvovec(msra[psrno], msdec[psrno], icra, icdec)
+    decdf = list(np.abs(np.subtract(icdec, msdec[psrno])))
+    nuind = []
+    for i in range(0, len(decdf)):
+        if decdf[i] < declcut:
+            nuind.append(i)
+            #decdf.pop(i)
+            #aang[i] = -1
+    fdecdf = list(np.abs(np.subtract(icdec[nuind], msdec[psrno])))
+    return [nuind, decdf]
+
+def S_ij(psrno, nuind): 
+
+    f'''
+    Returns the signal PDF for the {psrno}th pulsar 
+    and the all neutrinos within the specified declcut as given in {nuind}
+    '''
+
+    ang2 = hvovec(msra[psrno], msdec[psrno], icra[nuind], icdec[nuind], rad=True) ** 2
+    sg = np.deg2rad(icang[nuind]) ** 2
+    return np.exp(-1 * ang2 / (2 * sg)) / (2 * np.pi * sg)
+
+def bgs(psrno, cone, twopi = False):
+
+    '''
+    Parameters
+    ----------
+    psrno : int
+        Pulsar number from the ATNF catalogue.
+    
+    cone : float
+        Solid angle cone in degrees.
+
+    twopi : Boolean, optional
+        (default) False -> The background PDF is NOT divided by 2pi
+        True -> The background PDF is divided by 2pi
+    
+    Returns
+    -------
+    Returns the background PDF for a given pulsar {psrno} within\n 
+    a solid angle cone of {cone} degrees for SINGLE STACKED ANALYSIS
+    '''
+
+    if twopi == True:
+        s_ang = (np.sin(np.deg2rad(msdec[psrno] + cone)) - np.sin(np.deg2rad(msdec[psrno] - cone)))*(2 * np.pi)
+        return 1/s_ang
     else:
-        count = 0
-        for j in aang:
-            if j != -1:
-                if j < cut:
-                    count += 1
-        return count
-#Ns = nsang(aang)
-
-
-def bg(i, cone, Ns):
-    #Calculating total no of neutrino events within delta +/- cone of ith PULSAR (not neutrino)
-    count = np.count_nonzero(np.abs(np.subtract(msdec[i],icdec))<cone)
-    #calculating solid angle with lt deg conesxipy
-    sang = Ns * 2 * np.pi * (np.sin(np.deg2rad(msdec[i] + cone)) - np.sin(np.deg2rad(msdec[i] - cone)))
-    return count/sang
-
-
-#B = bg(psrno)
+        s_ang = (np.sin(np.deg2rad(msdec[psrno] + cone)) - np.sin(np.deg2rad(msdec[psrno] - cone)))
+        return 1/s_ang
 
 def Pr(x, Ns, S, B):
+
+    '''
+    Parameters
+    ----------
+    x : int
+        Assumed no.of associated events
+
+    Ns : int
+        No.of neutrinos used for analysis
+
+    S : float
+        Signal PDF
+    B : float
+        Background PDF
+    
+    Returns
+    -------
+    float 
+        Returns the probability of the selected set of neutrinos being associated\n
+        with a given pulsar with {Ns} neutrinos, {S} signal and {B} background PDF and {x} assumed associated events
+    '''
+
     nsN = x/Ns
     return np.add(np.multiply(nsN , S), np.multiply(np.subtract(1, nsN), B))
 
-def TS(i, x, S, B, Ns):
-    if len(S) < p:
-        return 2*np.sum(np.log(Pr(x,  Ns[i], S[i], B[i])/B[i]))
-    else:
-        return 2*np.sum(np.log(Pr(x,  Ns, S, B)/B))
-#TS = lambda i, x: 
 
-def TSmax(i, S, B, Ns):
+def TS(i, x, S, B, Ns):
+
+    '''
+    Returns the Test Stastic value fot {i}^th grb
+    at $n_s$ = {x} for its parameters S, B, Ns
+    '''
+    
+    return 2*np.sum(np.log(Pr(x,  Ns, S, B)/B))
+
+
+def ns_for_TSmax(i, S, B, Ns):
+
+    '''
+    Parameters
+    ----------
+    i : int
+        Pulsar number from the ATNF catalogue.
+
+    Ns : int
+        No.of neutrinos used for analysis
+
+    S : float ->        Signal PDF
+
+    B : float ->       Background PDF
+
+    Returns
+    -------
+    Returns the value of $n_s$ for which
+    the TS is maximum for {i}^th grb
+    '''
+
     #returns the TSmax for i^th GRB
-    #a = []
-    #for j in range(1000):
-        #a.append(TS(i,j))
-         
-    #init_guess = a.index(max(a))
     nll = lambda x: -TS(i,x, S, B, Ns)
-    soln = minimize(nll, 3 ,bounds=((0,None),))#313, 314, 349, 370, 374, 378, 381, 394, 396, 397, 423, 434
+    soln = minimize(nll, 3 ,bounds=((0,None),))
     ns = float(soln.x)
-    #print(f"Pulsar {i} done")
     return ns
+
+def plotpsr(psrno = 0):
+    
+    '''
+    Parameters
+    ----------
+    psrno : int
+        Pulsar number from the ATNF catalogue.
+
+    Returns:    None\n
+    Plots the signal PDF for the {psrno}th pulsar and\n
+    prints the maxns and TSmax for the same
+
+    '''
+
+
+
+    cut = 5
+    cone = 5
+    nuind, decdf = angdecfinder(psrno, cut)
+    S = S_ij(psrno, nuind)
+    B = bgs(psrno, cone)
+    Ns = len(nuind)
+    tns=[TS(psrno, i, S, B, Ns) for i in range(0, 1000)]
+    print(Ns)
+    plt.plot(range(0, 1000), tns)
+    plt.show()
+    print([ns_for_TSmax(psrno, S, B, Ns), TS(psrno, ns_for_TSmax(psrno, S, B, Ns), S, B, Ns)])
